99c66779f5f757a102c47e85af85ae89693c249e
sujanyaD <soujanya.gh@gmail.com>
2024-04-14 21:51:15 -0400

    third part

--- script.js
+++ script.js
@@ -36,11 +36,10 @@ function isPrime(num) {
 //         console.log(j + 1 + " is prime");
 //         break;
 //     }
-
 // }
-let n = 11;
-let j= n;
-while ( j < j + 1) {
+let n = 10;
+let j = n;
+while (j < j + 1) {
 
     if (isPrime(j + 1)) {
         console.log(j + 1 + " is prime");
@@ -48,6 +47,46 @@ while ( j < j + 1) {
     }
     j++;
 }
+console.log("================================================")
+
+// Feeling Loopy
+
+let id = ["42", "57", "63", "98"];
+let name = ["Bruce", "Bob", "Blaine", "Bill"];
+let Occupation = ["Knight", "Fry Cook", "Quiz Master", "Doctors Assistant"];
+let age = ["41", "19", "58", "26"];
+let str4 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+
+let cell1 = "";
+let data = [];
+
+for (let i=0; i<str4.length;i++) {
+    if (str4[i] != ',' && str4[i]!='\n') {
+        cell1 = cell1 + str4[i];
+        if(i==str4.length-1){
+            data.push(cell1);
+            break;
+        }
+    }
+    else if (str4[i] == '\n')
+    {
+        data.push(cell1);
+        cell1 = "";
+    }
+    else
+    {
+        data.push(cell1);
+        cell1 = "";
+    }
+
+}
+console.log(data[0],data[1],data[2],data[3]);
+console.log(data[4],data[5],data[6],data[7]);
+console.log(data[8],data[9],data[10],data[11]);
+console.log(data[12],data[13],data[14],data[15]);
+console.log(data[16],data[17],data[18],data[19]);
+
 
 
 
