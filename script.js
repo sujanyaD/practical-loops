9f1adbff1a5e67f2435a745828c2cc5ad853a5f9
sujanyaD <soujanya.gh@gmail.com>
2024-04-14 22:41:21 -0400

    made changes in code

--- script.js
+++ script.js
@@ -51,16 +51,11 @@ console.log("================================================")
 
 // Feeling Loopy
 
-let id = ["42", "57", "63", "98"];
-let name = ["Bruce", "Bob", "Blaine", "Bill"];
-let Occupation = ["Knight", "Fry Cook", "Quiz Master", "Doctors Assistant"];
-let age = ["41", "19", "58", "26"];
-let str4 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-
+//let str4 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+let str4 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 let cell1 = "";
 let data = [];
-
 for (let i=0; i<str4.length;i++) {
     if (str4[i] != ',' && str4[i]!='\n') {
         cell1 = cell1 + str4[i];
@@ -79,8 +74,8 @@ for (let i=0; i<str4.length;i++) {
         data.push(cell1);
         cell1 = "";
     }
-
 }
+
 console.log(data[0],data[1],data[2],data[3]);
 console.log(data[4],data[5],data[6],data[7]);
 console.log(data[8],data[9],data[10],data[11]);
